f2611725ace999fafc2c1b7f3169c9a478d66643
thehxdev <hossein.khosravi.ce@gmail.com>
2024-12-12 23:32:04 +0330

    implemented multi-buffer allocator
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- arena.h
+++ arena.h
@@ -68,55 +68,80 @@ int arena_destroy(Arena_t *arena);
 
 #ifdef LIBARENA_ARENA_IMPLEMENTATION
 
+#include <stdint.h>
+#include <assert.h>
+
 #ifndef NULL
     #define NULL ((void*)0)
 #endif /* NULL */
+
 #define PTR_SIZE    (sizeof(void*))
 
+#ifdef LIBARENA_PLAT_UNIX
+    #define __ALLOC(size)   \
+        mmap(NULL, (size), PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANON, -1, 0)
+#else
+    #define __ALLOC(size)   \
+        VirtualAlloc(NULL, (size), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+#endif
+
+#define __calc_free_space(ptr, end) ((size_t)((end) - (ptr) - 1))
+
+
 struct __arena {
-    /* Buffer */
-    void *buf;
+    /* Pointer to first buffer */
+    void *base;
 
     /* Pointer to top of buffer */
     void *ptr;
 
     /* End of buffer */
     void *end;
+
+    /* Remaining free bytes in current buffer */
+    size_t free;
+
+    /* Size of buffer */
+    size_t size;
 };
 
 Arena_t arena_new(unsigned long size) {
     Arena_t a;
-#ifdef LIBARENA_PLAT_UNIX
-    a.buf = mmap(NULL, size, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANON, -1, 0);
-#else
-    a.buf = VirtualAlloc(NULL, size, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-#endif
-    a.ptr = a.buf;
-    a.end = a.buf + size;
+    a.base = __ALLOC(size);
+    assert(a.base && "base buffer is NULL");
+    a.ptr = a.base;
+    a.end = a.base + size - PTR_SIZE;
+    a.size = size;
+    a.free = __calc_free_space(a.ptr, a.end);
     return a;
 }
 
 void *arena_alloc(Arena_t *arena, unsigned long size) {
-    void *p = arena->ptr;
-    /* TODO: At this time the buffer is not growable.
-     * Handle this edge cases by allocating new buffer and extending
-     * original buffer by storing new buffer's address in last bytes
-     * of original buffer */
-    if ((p + size) >= (arena->end - PTR_SIZE - 1))
+    if (arena->ptr + size >= arena->end) {
+#ifdef LIBARENA_MULTI_BUFFER
+        arena->ptr = __ALLOC(arena->size);
+        assert(arena->ptr && "allocated buffer is NULL");
+        *((uintptr_t*)arena->end) = (uintptr_t)arena->ptr;
+        arena->end = arena->ptr + arena->size - PTR_SIZE;
+#else
         return NULL;
+#endif
+    }
+    void *p = arena->ptr;
     arena->ptr += size;
+    arena->free = __calc_free_space(arena->ptr, arena->end);
     return p;
 }
 
 void arena_clear(Arena_t *arena) {
-    arena->ptr = arena->buf;
+    arena->ptr = arena->base;
 }
 
 int arena_destroy(Arena_t *arena) {
 #ifdef LIBARENA_PLAT_UNIX
-    return (!munmap(arena->buf, (unsigned long)arena->end-(unsigned long)arena->buf));
+    return (!munmap(arena->base, arena->size));
 #else
-    return VirtualFree(arena->buf, 0, MEM_RELEASE);
+    return VirtualFree(arena->base, 0, MEM_RELEASE);
 #endif
 }
 
