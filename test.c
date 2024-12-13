01d150d4c57a469f3d9151cd9b16137b30e6c53a
thehxdev <hossein.khosravi.ce@gmail.com>
2024-12-13 12:36:42 +0330

    updated tests
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- test.c
+++ test.c
@@ -2,14 +2,15 @@
 #include <assert.h>
 
 #define LIBARENA_ARENA_IMPLEMENTATION
+#define LIBARENA_MULTI_BUFFER
 #include "arena.h"
 
-#define XS_LEN  (5)
+#define XS_LEN  (20)
 
 int main(void) {
     int i;
     Arena_t a = arena_new(LIBARENA_DEFAULT_SIZE);
-    assert(a.buf != NULL && "buffer is NULL");
+    assert(a.base != NULL && "buffer is NULL");
 
     int *xs = (int*)arena_alloc(&a, sizeof(int) * XS_LEN);
     assert(xs != NULL && "allocation for 10 ints returned NULL");
@@ -18,9 +19,9 @@ int main(void) {
     for (i = 0; i < XS_LEN; i++)
         xs[i] = i;
 
-    for (int i = 0; i < XS_LEN; i++)
+    for (i = 0; i < XS_LEN; i++)
         printf("%d\n", xs[i]);
 
-    assert(arena_destroy(&a) && "arena_destroy failed");
+    arena_destroy(&a);
     return 0;
 }
